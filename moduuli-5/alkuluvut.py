f4173d14bb2c900a52ce62575b9bb07101aef09f
Hendalf <stasik.mihailov06@gmail.com>
2026-08-30 20:30:24 +0300

    Moduuli 5 tehty kaikki

---
+++ moduuli-5/alkuluvut.py
@@ -0,0 +1,8 @@
+luku = int(input("Anna luku: "))
+
+for i in range(2, luku):
+    if luku % i == 0:
+        print("Luku ei ole alkuluku.")
+        break
+else:
+    print("Luku on alkuluku.")
---
+++ moduuli-5/kaupungit.py
@@ -0,0 +1,8 @@
+kaupungit = []
+
+for i in range(0, 5):
+    kaupunki = input("Anna kaupunki: ")
+    kaupungit.append(kaupunki)
+    
+for kaupunki in kaupungit:
+    print(kaupunki)
